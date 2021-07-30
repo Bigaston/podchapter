8b3ee7e8cab452bb963837d633d5ef4ccc42c381
Bigaston <bonsoir@bigaston.me>
2021-07-30 20:23:50 +0200

    Ajout d'electron et du build

---
+++ electron/main.ts
@@ -0,0 +1,52 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+import * as isDev from 'electron-is-dev';
+
+let win: BrowserWindow | null = null;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  if (isDev) {
+    win.loadURL('http://localhost:3000/index.html');
+  } else {
+    // 'build/index.html'
+    win.loadURL(`file://${__dirname}/../index.html`);
+  }
+
+  win.on('closed', () => win = null);
+
+  // Hot Reloading
+  if (isDev) {
+    // 'node_modules/.bin/electronPath'
+    require('electron-reload')(__dirname, {
+      electron: path.join(__dirname, '..', '..', 'node_modules', '.bin', 'electron'),
+      forceHardReset: true,
+      hardResetMethod: 'exit'
+    });
+  }
+
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
